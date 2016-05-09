26a8f7c90e3c0499728223d4c35758cc75668444
Bulk Developer <jaydenpatrick1110@gmail.com>
2016-05-09 19:26:22 -0400

    Update Main.php

--- src/CrateKeys/Main.php
+++ src/CrateKeys/Main.php
@@ -27,7 +27,7 @@ $this->getServer()->getLogger()->info(TextFormat::BLUE."[CrateKeys] Enabled!");
 public function onCrateTap(PlayerInteractEvent $event){
     $config = $this->getConfig();
 		$player = $event->getPlayer();
-		$key = $event->getItem();
+		$key = $event->getItem()->getId();
 		$crate = $event->getBlock()->getId();
 		if($key == $config->get("key")){
 			if($crate == $config->get("crate")){
